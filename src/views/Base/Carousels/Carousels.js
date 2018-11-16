6debceeca0b2ba3d0dfee8c54b76aa7a0f94cc47
Jose Jimenez <joseajimenez@me.com>
2018-11-16 15:34:31 -0500

    testing

---
+++ src/views/Base/Carousels/Carousels.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Card, CardBody, CardHeader, Carousel, CarouselCaption, CarouselControl, CarouselIndicators, CarouselItem, Col, Row } from 'reactstrap';
+import Widget01 from '/Users/JoseJimenez/Downloads/coreui-free-react-admin-template-master/my-project/src/views/Widgets/Widget01';
+import SwipeableViews from 'react-swipeable-views';
+
+
+const mystyles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+  },
+  slide1: {
+    background: '#FEA900',
+  },
+  slide2: {
+    background: '#B3DC4A',
+  },
+  slide3: {
+    background: '#6AC0FF',
+  },
+};
+
+class Carousels extends Component {
+/*
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+*/
+  render() {
+    const { activeIndex } = this.state;
+
+    const MyComponent = () => (
+      <SwipeableViews>
+        <div style={Object.assign({}, mystyles.slide, mystyles.slide1)}>
+          slide n°1
+        </div>
+        <div style={Object.assign({}, mystyles.slide, mystyles.slide2)}>
+          slide n°2
+        </div>
+        <div style={Object.assign({}, mystyles.slide, mystyles.slide3)}>
+          slide n°3
+        </div>
+      </SwipeableViews>
+    );
+
+    return (
+      <div className="animated fadeIn">
+        <MyComponent/>
+      </div>
+    );
+  }
+}
+
+export default Carousels;
